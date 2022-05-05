2c356311c08f818e6b605f1b530ce098b9269e4c
Junsang(Jason) Yoo <jyoo2649@gmail.com>
2022-05-04 22:48:31 -0700

    Trying to figure out mysql syntax error on add function

--- scripts/users.js
+++ scripts/users.js
@@ -68,15 +68,15 @@ app.post('/add-user', (req, res) => {
     });
 
     db.connect();
-
-    db.query('INSERT INTO user (name, email, password) vlaues(?, ?, ?)',
+    // console.log("name: ", req.body.name, " email: ", req.body.email, " password: ", req.body.password);
+    db.query('INSERT INTO user (`name`, `email`, `password`) vlaues(?, ?, ?)',
                 [req.body.name, req.body.email, req.body.password],
                 (error, results, fields) => {
                     if (error) console.log(error);
-                    console.log('Rows reutrned are: ', results);
+                    // console.log('Rows reutrned are: ', results);
                     res.send({ status: "success", msg: "Record added."});
                 });
-                connection.end();
+                db.end();
 });
 
 app.post('/update-user', (req, res) => {
