a305ca8ae3f40805940b9f3989e6a3352159a978
Junsang(Jason) Yoo <jyoo2649@gmail.com>
2022-05-05 17:59:23 -0700

    Delete users by id in users.html and users.js

--- scripts/users.js
+++ scripts/users.js
@@ -9,7 +9,7 @@ app.use("/css", express.static("../styles"));
 
 app.get('/', function (req, res) {
 
-    const db = mysql.createConnection({
+    const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
@@ -25,30 +25,30 @@ app.get('/', function (req, res) {
         password varchar(20),
         PRIMARY KEY (ID));`;
     
-    db.connect();
-    db.query(sql, (error, results) => {
+    connection.connect();
+    connection.query(sql, (error, results) => {
         if (error) console.log(error);
         console.log(results);
     });
-    db.end();
+    connection.end();
     let doc = fs.readFileSync('../users.html', "utf-8");
     res.send(doc);
 });
 
 app.get('/get-users', (req, res) => {
-    const db = mysql.createConnection({
+    const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'talkit'
     });
-    db.connect();
-    db.query('SELECT * FROM user', (error, results) => {
+    connection.connect();
+    connection.query('SELECT * FROM user', (error, results) => {
         if (error) console.log(error);
         console.log('Rows returned are: ', results);
         res.send({ status: "success", rows: results });
     });
-    db.end();
+    connection.end();
 });
 
 app.use(express.json());
@@ -61,44 +61,66 @@ app.post('/add-user', (req, res) => {
     console.log("Email", req.body.email);
     console.log("Password", req.body.password);
 
-    let db = mysql.createConnection({
+    let connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'talkit'
     });
 
-    db.connect();
+    connection.connect();
     // console.log("name: ", req.body.name, " email: ", req.body.email, " password: ", req.body.password);
-    db.query('INSERT INTO user (name, email, password) values(?, ?, ?)',
+    connection.query('INSERT INTO user (name, email, password) values(?, ?, ?)',
                 [req.body.name, req.body.email, req.body.password],
                 (error, results, fields) => {
                     if (error) console.log(error);
                     // console.log('Rows reutrned are: ', results);
                     res.send({ status: "success", msg: "Record added."});
                 });
-                db.end();
+    connection.end();
 });
 
 app.post('/update-user', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    let db = mysql.createConnection({
+    let connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'talkit'
     });
-    db.connect();
+    connection.connect();
     // console.log("Update values id: ", req.body.id, " user name: ", req.body.name, " email: ", req.body.name, " password: ", req.body.password)
-    db.query('UPDATE user SET name = ?, email = ?, password = ? WHERE ID = ?',
+    connection.query('UPDATE user SET name = ?, email = ?, password = ? WHERE ID = ?',
                 [req.body.name, req.body.email, req.body.password, req.body.id],
                 (error, results) => {
-                    if (error) console.log(eror);
+                    if (error) console.log(error);
 
-                    res.send({ status: "succes", msg: "Recorded updated."});
+                    res.send({ status: "success", msg: "Recorded updated."});
                 });
-                db.end();
+    connection.end();
+})
+
+app.post('/delete-user', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'talkit'
+    });
+    connection.connect();
+    let deleteSql = `DELETE 
+                    FROM user 
+                    WHERE ID =?`;
+    connection.query(deleteSql, 
+        [req.body.id],
+        (error, results)=> {
+            if (error) console.log(error);
+            res.send({ status: "success", msg: "Record deleted"})
+        })
+    connection.end();
 })
 
 let port = 8000;
