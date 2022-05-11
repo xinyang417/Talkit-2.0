2e229be9c75aea683457253502737fc997139358
RichardOhata <richardohata@gmail.com>
2022-05-10 19:39:44 -0700

    Changed code for printing out PORT

--- app.js
+++ app.js
@@ -530,8 +530,5 @@ async function init() {
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}.');
+    console.log('Server is running on port ' + PORT + ' .');
 });
-
-
-
--- config/db.config.js
+++ config/db.config.js
@@ -4,5 +4,3 @@ user: "b459ce75b586dd",
 password: "7790c83a",
 database: "heroku_7ab302bab529edd"
 };
-
-//mysql://b459ce75b586dd:7790c83a@us-cdbr-east-05.cleardb.net/heroku_7ab302bab529edd?reconnect=true
