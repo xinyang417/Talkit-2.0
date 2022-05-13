0654531ccbe521cbb9ad024ff53e6fc104a62ebc
RichardOhata <richardohata@gmail.com>
2022-05-13 00:42:19 -0700

    Changed port number of hosting website

--- app.js
+++ app.js
@@ -644,7 +644,7 @@ handleDisconnect();
 
 // RUN SERVER REMOTELY
 if (is_heroku) {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 8080;
     app.listen(PORT, () => {
         console.log('Server is running on port ' + PORT + ' .');
     });
