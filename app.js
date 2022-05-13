811ac704fe4aa93dfd227c772682e20d32f65e48
RichardOhata <richardohata@gmail.com>
2022-05-12 23:11:37 -0700

    Removed comments

--- app.js
+++ app.js
@@ -607,6 +607,14 @@ app.get("/logout", (req, res) => {
     }
 });
 
+
+/**
+ * Code for Connection Error handling while hosting.
+ * I found this code on stackoverflow.com.
+ * 
+ * @author https://stackoverflow.com/users/395659/cloudymarble
+ * @see https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection
+ */
 var connection;
 
 function handleDisconnect() {
@@ -617,16 +625,16 @@ function handleDisconnect() {
   connection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+      setTimeout(handleDisconnect, 2000); 
+    }                                     
+  });                                     
+                                          
   connection.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                        
+    } else {                                      
+      throw err;                                  
     }
   });
 }
