922727d160e9a4c412d98685729220fe0ba6252b
RichardOhata <richardohata@gmail.com>
2022-05-18 18:11:46 -0700

    Added logout confirmation modal in all relevant html pages

--- js/story_comment.js
+++ js/story_comment.js
@@ -111,3 +111,29 @@ function deleteComment(commentID) {
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.send("commentID=" + commentID);
 }
+
+// Modal Functions
+var modal = document.getElementById('simpleModal');
+
+var modalBtn = document.getElementById('logout');
+
+var goBack = document.getElementById('modal-return');
+
+
+modalBtn.addEventListener('click', showModal);
+goBack.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
+
+function showModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
--- js/update-profile.js
+++ js/update-profile.js
@@ -129,3 +129,30 @@ function displayName() {
 }
 
 displayName();
+
+
+// Modal Functions
+var modal = document.getElementById('simpleModal');
+
+var modalBtn = document.getElementById('logout');
+
+var goBack = document.getElementById('modal-return');
+
+
+modalBtn.addEventListener('click', showModal);
+goBack.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
+
+function showModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
