152c48f7a1a79bed898fb19bafd5ae4f27acf05e
Chi Lan <chilanhuynh@hotmail.com>
2022-05-20 01:06:16 -0700

    Added Easter Egg to all pages.

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -1,14 +1,16 @@
-var clickCount = 0;
-        var maxCount = 9;
+"use strict";
 
-        function showEasterEgg() {
-            clickCount++;
-            if (clickCount == maxCount) {
-                alert("Are you looking for us?");
-                var imgEE = document.getElementById("easterEgg");
-                imgEE.style.display = 'block';
-            }
-            setTimeout(function () {
-                document.getElementById('eEgg').style.display = 'none'
-            }, 12000);
-        }
+var clickCount = 0;
+var maxCount = 9;
+
+function showEasterEgg() {
+    clickCount++;
+    if (clickCount == maxCount) {
+        alert("Are you looking for us?");
+        var imgEE = document.getElementById("easterEgg");
+        imgEE.style.display = 'block';
+    }
+    setTimeout(function () {
+        document.getElementById('eEgg').style.display = 'none'
+    }, 12000);
+}
