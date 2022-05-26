ae668101d96ed87da920421d3f71ccef5182cce0
Junsang(Jason) Yoo <jyoo2649@gmail.com>
2022-05-25 20:29:12 -0700

    Added message features on web version.

--- js/messages.js
+++ js/messages.js
@@ -9,15 +9,17 @@ function openChatWindow(room) {
     
     if (!document.getElementById(room)) {
         document.getElementById("chat-box").innerHTML = `<div class="chat-window" id="${room}"></div>`;
-        
+        document.getElementById("dchat-box").innerHTML = `<div class="chat-window" id="d${room}"></div>`;
     }
     document.getElementById("send").setAttribute("onclick", `sendMessage('${room}')`);
+    document.getElementById("dsend").setAttribute("onclick", `sendMessage('${room}')`);
 }
 function createRoom(id, name) {
     let loggedInUser = JSON.parse(sessionStorage.getItem('user'));
     room = Date.now() + Math.random();
     room = room.toString().replace(".", "_");
     document.getElementById("friend-dName").getElementsByTagName("b")[0].innerHTML = name;
+    document.getElementById("dfriend-dName").getElementsByTagName("b")[0].innerHTML = name;
     socket.emit('create', {room: room, userId: loggedInUser.ID, withUserId: id, hostName: data.displayName});
     openChatWindow(room);
 }
@@ -32,7 +34,7 @@ function sendMyMessage(chatWindowId, fromUser, message) {
                     </div>
                 </div>`;
     } else {
-        html = `<div class="userBubble">
+        html = `<div class="userBubble"> 
                     <div class="userMsg">
                         <p class="userText">${message}</p>
                         <img class="userPic" src="/img/${fromUser.profilePic}" />
@@ -40,12 +42,18 @@ function sendMyMessage(chatWindowId, fromUser, message) {
                 </div>`;
     }
     
-    document.getElementById(`${chatWindowId}`).insertAdjacentHTML("beforeend", html); 
+    document.getElementById(`${chatWindowId}`).insertAdjacentHTML("beforeend", html);
+    document.getElementById(`d${chatWindowId}`).insertAdjacentHTML("beforeend", html);  
 }
 
 function sendMessage(room) {
     let message = document.getElementById("msg").value;
     document.getElementById("msg").value = "";
+    let msg = document.getElementById("dmsg").value;
+    document.getElementById("dmsg").value = "";
+    if(msg) {
+        message = msg;
+    }
     socket.emit('send-message', {room: room, message:message, from: data});
     sendMyMessage(room, data, message);
 }
@@ -53,10 +61,12 @@ function sendMessage(room) {
 socket.on('updateUserList', (userList) => {
     let loggedInUser = JSON.parse(sessionStorage.getItem('user'));
     document.getElementById("inbox-component").innerHTML = `<ul></ul>`;
+    document.getElementById("dinbox-component").innerHTML = `<ul></ul>`;
     let i = 0;
     userList.forEach(item => {
         if (loggedInUser.ID != item.ID){
             document.getElementById('inbox-component').getElementsByTagName("ul")[i].innerHTML = `<li data-id ="${item.ID}" onclick = "createRoom('${item.ID}', '${item.displayName}')">${item.displayName}</li>`;
+            document.getElementById('dinbox-component').getElementsByTagName("ul")[i].innerHTML = `<li data-id ="${item.ID}" onclick = "createRoom('${item.ID}', '${item.displayName}')">${item.displayName}</li>`;
             i++;
         }
     });
@@ -64,11 +74,11 @@ socket.on('updateUserList', (userList) => {
 
 socket.on('invite', function(data) {
     document.getElementById("friend-dName").getElementsByTagName("b")[0].innerHTML = `${data.hostName}`;
+    document.getElementById("dfriend-dName").getElementsByTagName("b")[0].innerHTML = `${data.hostName}`;
     socket.emit("joinRoom", data)
 });
 
 socket.on('receive-message', (data) => {
-    console.log("receive message from server");
     if(!document.getElementById(`${data.room}`)) {
         openChatWindow(data.room);
     }
@@ -79,8 +89,6 @@ socket.on('receive-message', (data) => {
 
 
 
-if (jQuery(window).width() < 701) {
-    console.log("width lower than 701");
     function openTab(evt, tabName) {
         // Declare all variables 
         var i, tabcontent, tablinks;
@@ -103,7 +111,6 @@ if (jQuery(window).width() < 701) {
     }
     // document.getElementById("inbox-tab").setAttribute("onclick", `openTab(event, 'inbox-component)`);
     // document.getElementById("chat-tab").setAttribute("onclick",`openTab(event, 'chat-component')`);
-}
 
 // Logout Modal Functions
 var modal = document.getElementById('simpleModal');
